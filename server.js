b98d2d20499233c28c674067943e2021892e2855
indersingh <ishansingh9079@gmail.com>
2024-03-20 18:05:08 +0530

    today

--- server.js
+++ server.js
@@ -20,9 +20,29 @@ app.get("/products/:category", (req, res) => {
 })
 app.get("/:price", (req, res) => {
     const price = req.params.price;
-    // console.log(price);
     const p = products.filter(p => p.price <= price)
     res.send(p)
+})
+
+// app.get("/products/searchkey", (req, res) => {
+//     const searchkey = req.query.searchkey;
+//     const f = products.filter((i) =>
+//         i.description.include(searchkey).toLocaleLowerCase ||
+//         i.title.include(searchkey).toLocaleLowerCase
+//     );
+//     res.send(f);
+// })
+app.get('/productss/searchkey', (req, res) => {
+    const searchkey = req.query.searchkey;
+    const filtered = products.filter((item) => item.description.toLowerCase().includes(searchkey) || item.title.toLowerCase().includes(searchkey));
+    res.send(filtered)
+});
+
+app.get("/products/kearch/searchkey", (req, res) => {
+    const searchkey = req.query.brand;
+    const k = req.query.cat;
+    const p = products.filter((product) => product.brand.toLowerCase().includes(searchkey) && product.category.toLowerCase().includes(k))
+      res.send(p)
 })
 
 app.listen(port, () => {
